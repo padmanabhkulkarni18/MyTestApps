2dfd19e56ec30e1e5f0691d10f81af283bf32527
ULP2KOR <ULP2KOR@BMH1124794.bmh.apac.bosch.com>
2020-10-13 19:02:27 +0530

    FileFormatChangeCode

--- Test/src/test/list/FloatDoubleTest.java
+++ Test/src/test/list/FloatDoubleTest.java
@@ -7,8 +7,12 @@ public class FloatDoubleTest {
 		double b = 0.9;
 		System.out.println("a : " + a);
 		System.out.println("a : " + a*10);
+		
 		System.out.println("b : " + b);
-		System.out.println("b : " + (float)b * Math.pow(10, 1));
+		System.out.println("b : " + b * Math.pow(10, 1));
+		b= b* Math.pow(10, 1);
+		System.out.println((int) b);
+//		System.out.println("Double : "+  b*);
 		
 	}
 
